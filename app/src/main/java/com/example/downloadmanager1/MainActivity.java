e535c3302f103155bb3d9604f11600d22bb6efff
Ankit Bajaj <ankitbajaj333@gmail.com>
2019-09-09 13:03:07 +0530

    Added Project files

---
+++ app/src/main/java/com/example/downloadmanager1/MainActivity.java
@@ -0,0 +1,124 @@
+package com.example.downloadmanager1;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.app.DownloadManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
+
+import java.io.File;
+import java.net.URI;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    EditText editText;
+    ImageView imageView;
+    private long downloadID;
+    private String url;
+
+    private BroadcastReceiver onDownloadComplete = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            //Fetching the download id received with the broadcast
+            long id = intent.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1);
+            //Checking if the received broadcast is for our enqueued download by matching download id
+            if (downloadID == id) {
+                Toast.makeText(MainActivity.this, "Download Completed", Toast.LENGTH_SHORT).show();
+            }
+
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.download);
+        editText = findViewById(R.id.etURL);
+        imageView = findViewById(R.id.imageView);
+        registerReceiver(onDownloadComplete, new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE));
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (
+                        ActivityCompat.checkSelfPermission(
+                                getBaseContext(),
+                                Manifest.permission.WRITE_EXTERNAL_STORAGE
+                        )
+                                == PackageManager.PERMISSION_GRANTED
+                ) {
+                    beginDownload();
+                } else {
+                    //else request permissions
+                    ActivityCompat.requestPermissions(
+                            MainActivity.this,
+                            new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE},
+                            12345
+                    );
+                }
+            }
+        });
+
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(onDownloadComplete);
+    }
+
+    private void beginDownload() {
+        File file = new File(getExternalFilesDir(null), "Dummy");
+        /*
+        Create a DownloadManager.Request with all the information necessary to start the download
+         */
+        url = editText.getText().toString();
+        if (!url.startsWith("http://") || !url.startsWith("https://")) {
+            url = "https://" + url;
+        }
+        DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url))
+                .setTitle("Dummy File")// Title of the Download Notification
+                .setDescription("Downloading")// Description of the Download Notification
+                .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE)// Visibility of the download Notification
+                .setDestinationUri(Uri.fromFile(file))// Uri of the destination file
+                //.setRequiresCharging(false)// Set if charging is required to begin the download
+                .setAllowedOverMetered(true)// Set if download is allowed on Mobile network
+                .setAllowedOverRoaming(true);// Set if download is allowed on roaming network
+        DownloadManager downloadManager = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
+        downloadID = downloadManager.enqueue(request);// enqueue puts the download request in the queue.
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 12345) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                beginDownload();
+
+            } else {
+                url = editText.getText().toString();
+                if (!url.startsWith("http://") || !url.startsWith("https://")) {
+                    url = "https://" + url;
+                }
+                Picasso.get().load(Uri.parse(url)).into(imageView);
+                Toast.makeText(this, "Does not have the required permission", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+}
